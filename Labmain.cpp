54b96f4b1a682d45013d1a73ee85867ee728be88
mllangon <mllangon@myuax.com>
2023-11-30 12:08:59 +0100

    Correccion de errores

--- Labmain.cpp
+++ Labmain.cpp
@@ -4,48 +4,53 @@
 using namespace std;
 
 bool is_filename_valid(const string& filename) {
-
-    return !filename.empty(); && filename.size < 256;
+    return !filename.empty() && filename.size() < 256;
 }
 
-void load_script(const char* filename, bool show_script = false){
-    if(!is_filename_valid(filename)){
-        cout << "Nombre no encontrado: " << endl;
+void load_script(const char* filename, bool show_script = false) {
+    if (!is_filename_valid(filename)) {
+        cout << "Nombre de archivo no válido." << endl;
+        return;
+    }
+
+    ifstream file(filename);
+    if (!file) {
+        cout << "Error al abrir el archivo: " << filename << " ." << endl;
         return;
     }
 
     string line;
-    while(getline(file, line)){
-        if(file.fail){
-            cout << "Error al leer el archivo" << filename << " ." << endl;
+    while (getline(file, line)) {
+        if (file.fail()) {
+            cout << "Error al leer el archivo: " << filename << " ." << endl;
             file.close();
             return;
         }
-        if(show_script){
+        if (show_script) {
             cout << line << endl;
         }
     }
 
-    if (file.bad()){
-        cerr<< "Error de lectura al final del archivo" << filename << " ." << endl;
-
+    if (file.bad()) {
+        cerr << "Error de lectura al final del archivo: " << filename << " ." << endl;
     }
     file.close();
 }
-void load_script(){
+
+void load_script() {
     string filename;
     cout << "Ingrese el nombre del archivo: ";
     getline(cin, filename);
 
-    if (!is_filename_valid(filename)){
-        cout << "Nombre no encontrado: " << endl;
+    if (!is_filename_valid(filename)) {
+        cout << "Nombre de archivo no válido." << endl;
         return;
     }
 
-    load.script(filename.c_str(), true);
+    load_script(filename.c_str(), true);
 }
 
-int main(){
+int main() {
     load_script();
     return 0;
 }
