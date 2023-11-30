35f6290e275f39226f83ab7c5c3b47b9783f3bb9
mllangon <mllangon@myuax.com>
2023-11-30 11:54:29 +0100

    Primer error

--- Labmain.cpp
+++ Labmain.cpp
@@ -7,3 +7,12 @@ bool is_filename_valid(const string& filename) {
 
     return !filename.empty(); && filename.size < 256;
 }
+
+void load_script(const char* filename, bool show_script = false){
+    if(!is_filename_valid(filename)){
+        cout << "Nombre no encontrado: " << endl;
+        return;
+    }
+
+
+}
