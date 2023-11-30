aae4ca068ff483c17a2436ec8129db4b398444da
mllangon <mllangon@myuax.com>
2023-11-30 12:17:12 +0100

    2.2

--- Solucion_Propuesta_Mejorada.cpp
+++ Solucion_Propuesta_Mejorada.cpp
@@ -14,3 +14,20 @@ struct ConsoleBox {
     void new_text() {/*...*/}
     void set_text(const string &text) { cout << text << endl; }
 };
+
+ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+
+bool is_filename_valid(const char* filename) {
+    if (!filename || std::strlen(filename) == 0 || std::strlen(filename) >= 500) {
+        return false;
+    }
+    // Añade aquí más reglas si es necesario.
+    return true;
+}
+
+void load_script(const char* filename, bool show_script = false) {
+    if (!is_filename_valid(filename)) {
+        cerr << "Nombre de archivo no válido o demasiado largo: " << filename << endl;
+        return;
+    }
+
