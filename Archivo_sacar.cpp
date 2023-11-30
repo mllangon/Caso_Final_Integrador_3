c92b9bf7b3507a14bbf0ba3e125f1de42cac9c5b
mllangon <mllangon@myuax.com>
2023-11-30 12:22:23 +0100

    archivo terminal

---
+++ Archivo_sacar.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int suma(int a, int b) {
+    return a + b;
+}
+int main() {
+    std::cout << suma(1, 2) << std::endl;
+
+    std::cout << "Este es un archivo para poder sacarlo a la terminal" << std::endl;
+    return 0;
+}
